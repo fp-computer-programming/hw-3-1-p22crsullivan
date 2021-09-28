ea02574bc1b47e3b43b5bbfbb761fb2bac1d8372
p22crsullivan <p22crsullivan@fairfieldprep.org>
2021-09-28 10:20:30 -0400

    Submit HW 3-1-1 and create HW 3-1-2

---
+++ hw3-1-2.py
@@ -0,0 +1,6 @@
+# Author: CRS 9/28/21
+first_card = input("Please enter your first card's value.")
+second_card = input("Please enter your second card's value.")
+total_value = first_card + second_card
+if total_value <= 21:
+    
