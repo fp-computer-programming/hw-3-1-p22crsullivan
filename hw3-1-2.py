72f389c982a77cb6500f6a8b502d17131edf1269
p22crsullivan <p22crsullivan@fairfieldprep.org>
2021-09-29 10:15:31 -0400

    Submit HW 3-1-2 and HW 3-1-3

--- hw3-1-2.py
+++ hw3-1-2.py
@@ -1,9 +1,6 @@
 # Author: CRS 9/28/21
-first_card = input("Please enter your first card's value.")
-second_card = input("Please enter your second card's value.")
-jack = 10
-queen = 10
-king = 10
+first_card = int(input("Please enter your first card's value."))
+second_card = int(input("Please enter your second card's value."))
 total_value = first_card + second_card
 if total_value <= 21:
     print("You are safe.")
---
+++ hw3-1-3.py
@@ -0,0 +1,8 @@
+# Author: CRS 9/29/21
+number = int(input("Please enter an integer."))
+if number % 2 == 0:
+    print("The number is a multiple of 2.")
+elif number % 3 == 0:
+    print("The number is a multiple of 3.")
+elif number % 5 == 0:
+    print("The number is a multiple of 5")
