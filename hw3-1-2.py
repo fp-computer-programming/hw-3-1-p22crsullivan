b3ca63f99272caf68c422159ec39626f54577e1a
p22crsullivan <p22crsullivan@fairfieldprep.org>
2021-09-29 09:35:26 -0400

    Add to HW 3-1-2

--- hw3-1-2.py
+++ hw3-1-2.py
@@ -1,6 +1,11 @@
 # Author: CRS 9/28/21
 first_card = input("Please enter your first card's value.")
 second_card = input("Please enter your second card's value.")
+jack = 10
+queen = 10
+king = 10
 total_value = first_card + second_card
 if total_value <= 21:
-    
+    print("You are safe.")
+else:
+    print("You busted.")
